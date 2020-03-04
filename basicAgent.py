4d22f7ff7060c2c595bf7e56e30f5b19f4c24351
Zain <zainsid11@gmail.com>
2020-03-04 14:13:03 -0500

    basic agent

--- basicAgent.py
+++ basicAgent.py
@@ -1,5 +1,15 @@
 from gameboard import Game
 from random import randrange, seed
+import numpy as np
+
+def is_number(a):
+    # will be True also for 'NaN'
+    try:
+        number = float(a)
+        return True
+    except ValueError:
+        return False
+
 class Tile:
     def __init__(self, x, y):
         self.x = x
@@ -90,28 +100,54 @@ def numNeighbors(self, x, y):
                 count = count + 1
     return count
 
+# def play(self):
+#     game = Game(4,2)
+#     old_grid = None
+#     current_grid = self.grid_display
+#     while not np.array_equal(current_grid,old_grid):
+#         old_grid = current_grid
+#         current_grid = basicAgent(game)
 
 def basicAgent(self):
     lose = False
-    temp = [randrange(self.gird_size), randrange(self.gird_size)]
+    #temp = [randrange(self.gird_size), randrange(self.gird_size)]
+    temp = (0,0)
     self.uncoverCell(temp[0], temp[1])
-    while ((lose == False) or (self.mine_count == flagCount)):
-        flagCount = 0
+    if (self.grid_display[temp[0]][temp[1]] == "*"):
+        lose= True
+
+    flagCount=0
+    while ((lose is False) and (self.mine_count != flagCount)):
+        #print("flagcount",flagCount)
         self.show()
+        print()
+        
         for x in range(self.gird_size):
+            
             for y in range(self.gird_size):
-                if (self.grid_display[x][y] != "#"): #Current cell is already revealed
+                #print(self.grid_display[x][y])
+                self.show()
+                print()
+                if (is_number(self.grid_display[x][y])): #Current cell is already revealed
                 # Case 1
+                    #print("here")
                     continue
-                elif (self.grid_display[x][y] == "F"): #Curret cell is already flagged
+                elif (str(self.grid_display[x][y]) is 'F'): #Curret cell is already flagged
                 # Case 2
                     flagCount = flagCount + 1
+                    #print("f inc", flagCount)
                     continue
-                elif (self.grid_display[x][y] == "M"): #Just reevealed current cell and its a mine :(
+                elif (str(self.grid_display[x][y]) is '*'): #Just reevealed current cell and its a mine :(
                 #print("Hit a Mine! You Lose!")
                     lose = True
+                    #print("here")
+                    break
                 else:
+                    self.uncoverCell(x,y)
                     clue = self.grid[x][y]
+                    if (self.grid_display[x][y] == "*"):
+                        lose= True
+                        break
                     if (clue == 0):
                         # All safe around cell
                         revealNeighbors(self, x, y)
@@ -124,8 +160,11 @@ def basicAgent(self):
                             revealNeighbors(self, x, y)
                         elif (clue == safeAroundCell(self, x, y)):
                             flagNeighbors(self, x, y)
-
-    if (lose == True):
+            
+            if (lose is True):
+                break
+    self.show()
+    if (lose is True):
         print("Mine hit")
     else:
         print("You won!")
@@ -179,7 +218,9 @@ print(stri)
 #print(curr.x, curr.y, curr.flag)
 
 
-game = Game(8, 8)
+game = Game(4,1)
+size = game.gird_size
+game.mineindicator(size)
 basicAgent(game)
 
 '''
